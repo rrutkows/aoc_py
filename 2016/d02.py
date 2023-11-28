c35f0cb49aa7bdc61d9d9370435a7c3e5fcadc77
Rafał Rutkowski <rrutkowski@gmail.com>
2023-11-28 10:25:13 +0100

    y2016 d02 p2

--- 2016/d02.py
+++ 2016/d02.py
@@ -1,6 +1,7 @@
 from util import data, runner
+from functools import reduce
 
-def solve(input):
+def solve01(input):
     directions = dict(U = (0, -1), D = (0, 1), L = (-1, 0), R = (1, 0))
     code = []
     for l in input.splitlines():
@@ -11,6 +12,27 @@ def solve(input):
         code.append(f"{y * 3 + x + 1}")
     return ''.join(code)
 
+def solve02(input):
+    directions = dict(U = 0, L = 1, D = 2, R = 3)
+    keypad = {
+        '1': '1131',
+        '2': '2263',
+        '3': '1274',
+        '4': '4384',
+        '5': '5556',
+        '6': '25A7',
+        '7': '36B8',
+        '8': '47C9',
+        '9': '9899',
+        'A': '6AAB',
+        'B': '7ADC',
+        'C': '8BCC',
+        'D': 'BDDD'
+    }
+    code = (reduce(lambda acc, c: keypad[acc][directions[c]], l, '5') for l in input.splitlines())
+    return ''.join(code)
+
 if __name__ == "__main__":
     input = data.get_input(2016, 2)
-    runner.run(lambda: solve(input))
+    runner.run(lambda: solve01(input))
+    runner.run(lambda: solve02(input))
